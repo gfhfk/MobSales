da9151b7a1ab0540da45e3f36ef16c211b45f188
o.metelskiy@hotmail.com <o.metelskiy@hotmail.com>
2014-01-09 11:49:27 +0200

    Template App

---
+++ MobSales/js/localization/dx.phonejs.de.js
@@ -0,0 +1,33 @@
+/*! 
+* DevExpress PhoneJS
+* Version: 13.2.5
+* Build date: Dec 3, 2013
+*
+* Copyright (c) 2012 - 2013 Developer Express Inc. ALL RIGHTS RESERVED
+* EULA: http://phonejs.devexpress.com/EULA
+*/
+
+Globalize.addCultureInfo("de", {messages: {
+        Yes: "Ja",
+        No: "Nein",
+        Cancel: "Abbrechen",
+        Clear: "Löschen",
+        Done: "Fertig",
+        Loading: "Laden...",
+        Select: "Auswählen...",
+        Search: "Suchen",
+        Back: "Zurück",
+        "dxLookup-searchPlaceholder": "Minimale Anzahl Zeichen: {0}",
+        "dxCollectionContainerWidget-noDataText": "Keine Daten verfügbar",
+        "dxList-pullingDownText": "Zum Aktualisieren nach unten ziehen",
+        "dxList-pulledDownText": "Zum Aktualisieren loslassen",
+        "dxList-refreshingText": "Aktualisiere...",
+        "dxList-pageLoadingText": "Laden...",
+        "dxListEditDecorator-delete": "Entfernen",
+        "dxScrollView-pullingDownText": "Zum Aktualisieren nach unten ziehen",
+        "dxScrollView-pulledDownText": "Zum Aktualisieren loslassen",
+        "dxScrollView-refreshingText": "Aktualisiere...",
+        "dxScrollView-reachBottomText": "Laden...",
+        "dxSwitch-onText": "EIN",
+        "dxSwitch-offText": "AUS"
+    }});
---
+++ MobSales/js/localization/dx.phonejs.ru.js
@@ -0,0 +1,33 @@
+/*! 
+* DevExpress PhoneJS
+* Version: 13.2.5
+* Build date: Dec 3, 2013
+*
+* Copyright (c) 2012 - 2013 Developer Express Inc. ALL RIGHTS RESERVED
+* EULA: http://phonejs.devexpress.com/EULA
+*/
+
+Globalize.addCultureInfo("ru", {messages: {
+        Yes: "Да",
+        No: "Нет",
+        Cancel: "Отмена",
+        Clear: "Очистить",
+        Done: "Готово",
+        Loading: "Загрузка...",
+        Select: "Выбрать...",
+        Search: "Поиск",
+        Back: "Назад",
+        "dxLookup-searchPlaceholder": "Минимальное количество символов: {0}",
+        "dxCollectionContainerWidget-noDataText": "Нет данных для отображения",
+        "dxList-pullingDownText": "Потяните, чтобы обновить...",
+        "dxList-pulledDownText": "Отпустите, чтобы обновить...",
+        "dxList-refreshingText": "Обновление...",
+        "dxList-pageLoadingText": "Загрузка...",
+        "dxListEditDecorator-delete": "Удалить",
+        "dxScrollView-pullingDownText": "Потяните, чтобы обновить...",
+        "dxScrollView-pulledDownText": "Отпустите, чтобы обновить...",
+        "dxScrollView-refreshingText": "Обновление...",
+        "dxScrollView-reachBottomText": "Загрузка...",
+        "dxSwitch-onText": "ВКЛ",
+        "dxSwitch-offText": "ВЫКЛ"
+    }});
