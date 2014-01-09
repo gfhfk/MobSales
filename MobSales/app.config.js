adad0410bb450579c645ade49bb362fa8925d150
o.metelskiy@hotmail.com <o.metelskiy@hotmail.com>
2014-01-09 14:39:06 +0200

    add config

---
+++ MobSales/app.config.js
@@ -0,0 +1,21 @@
+﻿window.MobSales = $.extend(true, window.MobSales, {
+    "config": {
+        "navigationType": "slideout",
+        "navigation": [
+            {
+                "id": "Home",
+                "title": "Home",
+                "action": "#Home",
+                "icon": "home",
+                "location": "navigation"
+            },
+            {
+                "id": "About",
+                "title": "About",
+                "action": "#About",
+                "icon": "about",
+                "location": "navigation"
+            }
+        ]
+    }
+});
